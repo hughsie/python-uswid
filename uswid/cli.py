c4c9d39c335fbced4474e3caa8caac0fd030b668
Richard Hughes <richard@hughsie.com>
2024-12-07 10:30:24 +0000

    Detect the version scheme when using --fixup

--- uswid/cli.py
+++ uswid/cli.py
@@ -242,6 +242,31 @@ def _type_for_fmt(
     return None
 
 
+def _contains_strict_subset(value: str, allowed: List[str]) -> bool:
+
+    for letter in value:
+        if letter not in allowed:
+            return False
+    return True
+
+
+def _detect_version_format_from_version(software_version: str) -> uSwidVersionScheme:
+
+    if _contains_strict_subset(
+        software_version, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    ):
+        return uSwidVersionScheme.DECIMAL
+    if _contains_strict_subset(
+        software_version, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+    ):
+        return uSwidVersionScheme.SEMVER
+    if _contains_strict_subset(
+        software_version, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-"]
+    ):
+        return uSwidVersionScheme.MULTIPARTNUMERIC
+    return uSwidVersionScheme.ALPHANUMERIC
+
+
 def _container_merge_from_filepath(
     container: uSwidContainer,
     base: uSwidFormatBase,
@@ -310,6 +335,10 @@ def _container_merge_from_filepath(
                 component.software_version = vcs.get_version()
                 if base.verbose:
                     fixup_strs.append(f"Add VCS version → {component.software_version}")
+            if not component.version_scheme and component.software_version:
+                component.version_scheme = _detect_version_format_from_version(
+                    component.software_version
+                )
             if not component.colloquial_version:
                 component.colloquial_version = vcs.get_commit()
                 if base.verbose:
