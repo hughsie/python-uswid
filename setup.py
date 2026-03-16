379c0c4e2f49293fe6f576c50552759e9e169d1b
Richard Hughes <richard@hughsie.com>
2026-03-16 15:17:57 +0000

    trivial: post release version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Contributors welcome, either adding new functionality or fixing bugs.
 
 setup(
     name="uswid",
-    version="0.6.0",
+    version="0.6.1",
     license="BSD-2-Clause-Patent",
     license_files=[
         "LICENSE",
